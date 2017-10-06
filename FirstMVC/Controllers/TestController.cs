2b19fc80ce6d5c5ded3cea30eb9dc97d026e6ae9
nrpopov <nrpopov@NPOPOV-10>
2017-10-06 17:47:54 +0300

    test

--- FirstMVC/Controllers/TestController.cs
+++ FirstMVC/Controllers/TestController.cs
@@ -1,17 +1,20 @@
-﻿using System;
+﻿using FirstMVC.Utils.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ViewModel;
 
 namespace FirstMVC.Controllers
 {
     public class TestController : Controller
     {
         // GET: Test
+        [HeaderFooterFilter]
         public ActionResult Index()
         {
-            return View();
+            return View(new BaseViewModel());
         }
     }
 }
---
+++ FirstMVC/Scripts/TestScript.js
@@ -0,0 +1,27 @@
+﻿//let test = "" + 1 + 0;
+//let devide = " -9\n" - 5;
+//let name = prompt('Full name:', '');
+//alert(name);
+//let isBoss = confirm("Are you the boss?");
+//alert(isBoss); // true if OK is pressed
+
+let userName = prompt('Username: ', '');
+if (userName == 'Admin') {
+    let password = prompt('Password:', '');
+    if (password == 'TheMaster') {
+        alert('Welcome');
+    }
+    else if (password == null) {
+        alert('Canceled');
+    }
+    else {
+        alert('Wrong password');
+    }
+}
+else if (userName == null) {
+    alert('Canceled');
+}
+else {
+    alert('I don’t know you');
+}
+
