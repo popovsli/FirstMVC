f4c55f2ee1af7b2c87d095443284069a43bcec7d
nrpopov <nrpopov@NPOPOV-10>
2017-11-24 16:43:18 +0200

    com

--- FirstMVC/Scripts/TestScript.js
+++ FirstMVC/Scripts/TestScript.js
@@ -5,23 +5,36 @@
 //let isBoss = confirm("Are you the boss?");
 //alert(isBoss); // true if OK is pressed
 
-let userName = prompt('Username: ', '');
-if (userName == 'Admin') {
-    let password = prompt('Password:', '');
-    if (password == 'TheMaster') {
-        alert('Welcome');
-    }
-    else if (password == null) {
-        alert('Canceled');
-    }
-    else {
-        alert('Wrong password');
-    }
-}
-else if (userName == null) {
-    alert('Canceled');
-}
-else {
-    alert('I don’t know you');
-}
+//let userName = prompt('Username: ', '');
+//if (userName == 'Admin') {
+//    let password = prompt('Password:', '');
+//    if (password == 'TheMaster') {
+//        alert('Welcome');
+//    }
+//    else if (password == null) {
+//        alert('Canceled');
+//    }
+//    else {
+//        alert('Wrong password');
+//    }
+//}
+//else if (userName == null) {
+//    alert('Canceled');
+//}
+//else {
+//    alert('I don’t know you');
+//}
 
+
+//while (true) {
+
+//    let number = +prompt("Enter number greater than 100 please :", '');
+
+//    if (number > 100 || number == 0) break;
+//}
+
+let number;
+do {
+    number = +prompt("Enter number greater than 100 please :", '');
+}
+while (number <= 100 && number)
